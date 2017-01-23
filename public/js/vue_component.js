163cef234075a892c07d1a1340c4030d292383cc
Jiwoopark <shapley_aldebaran@kaist.ac.kr>
2017-01-23 15:33:30 +0900

    first page

--- public/js/vue_component.js
+++ public/js/vue_component.js
@@ -1,23 +1,54 @@
 Vue.component('slide-element', {
-    props : ['text', 'isWhite'],
+    props : ['texts', 'isWhite'],
     template : `<section>
-        <main>
-            <slot name='main'></slot>
-        </main>             
-        <div class='card'>
-            <slot name='card'>{{text}}</slot>
+        <slot name='main'></slot>
+        <div class='card' v-for="talk in talks">
+            {{ talk }}
         </div>
+        <slot name='sub'></slot>
+
+        <!-- arrow prev & next -->
         <div class="arrow">
             <div class="swiper-prev">
-                <div class="up_arrow" v-if='isWhite'><img src="assets/up_arrow_gray.png" alt="arrowdown"> </div>
-                <div class="up_arrow" v-else><img src="assets/up_arrow.png" alt="arrowdown"></div>    
+                <div v-on:click="prev_step" class="up_arrow">
+                    <img :src="arrow[0]" alt="arrowup"> 
+                </div>
             </div>
             <div class="swiper-next">
-                <div class="down_arrow"  v-if='isWhite'><img src="assets/main_arrowdown_gray.gif" alt="arrowdown"> </div>
-                <div class="down_arrow" v-else><img src="assets/main_arrowdown.gif" alt="arrowdown"></div>
+                <div v-on:click="next_step" class="down_arrow">
+                    <img :src="arrow[1]" alt="arrowdown">
+                </div>
             </div>
         </div>
-    </section>`
+    </section>`,
+    data : function() {
+        return {
+            talks : [],
+        }
+    },
+    computed : {
+        arrow : function() {
+            var gray = ['assets/up_arrow_gray.png', 'assets/main_arrowdown_gray.gif'];
+            var white = ['assets/up_arrow.png', 'assets/main_arrowdown.gif'];
+            return this.isWhite ? gray : white;
+        }
+    },
+    methods : {
+        next_step : function() {
+            if (this.texts && this.texts.length > 0) {
+                this.talks.push(this.texts.shift());
+            } else {
+                mySwiper.slideNext();
+            }
+        }, 
+        prev_step : function() {
+            if (this.talks.length > 0) {
+                this.texts.splice(0, 0, this.talks.pop());
+            } else {
+                mySwiper.slidePrev();
+            }
+        }
+    }
 });
 
 var app = new Vue({
