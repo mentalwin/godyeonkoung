98085c2c1879276021d7176519810b45180264d9
Junhee Pyo <Junheee.p@gmail.com>
2017-02-08 11:28:30 +0900

    Junhee's work

---
+++ YK_tofu_build_2/resource/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/tofu', function(req, res, next){
+    res.render('tofu');
+});
+
+module.exports = router;
