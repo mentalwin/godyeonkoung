761b26f46d0fadc1c85a80a6bd724d76077e95e8
Jiwoopark <shapley_aldebaran@kaist.ac.kr>
2017-02-15 17:07:16 +0900

    redirect

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,3 +1,8 @@
 var useragent_redirect = window.navigator.userAgent;
 
+function checkUA() {
+    var ch = useragent_redirect.match(/Chrome/i);
+    var sa = useragent_redirect.match(/Safari/i);
 
+    if (!ch && !sa) window.location = "redirect";
+}
